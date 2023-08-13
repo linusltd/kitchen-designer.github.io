f5556a2679d3adde7d26f038dd6c10a75b021e9f
Hamza Ashraf <hamzaashraf160@gmail.com>
2023-08-13 17:10:50 +0500

    .

--- resources/views/admin/layouts/partials/navbar.blade.php
+++ resources/views/admin/layouts/partials/navbar.blade.php
@@ -9,12 +9,8 @@
 
     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
         <!-- Search -->
-        <div class="navbar-nav align-items-center">
-            {{-- <div class="nav-item d-flex align-items-center">
-                <i class="bx bx-search fs-4 lh-0"></i>
-                <input type="text" class="form-control border-0 shadow-none" placeholder="Search..."
-                    aria-label="Search..." />
-            </div> --}}
+        <div class="">
+            Visit Your Site =>  <a href="{{ route('website.home.index') }}" target="_blank">{{" "}} {{ route('website.home.index') }}</a>
         </div>
         <!-- /Search -->
 
@@ -28,63 +24,16 @@
 
             <!-- User -->
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
-                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-                    <div class="avatar avatar-online">
-                        <img src="{{ asset('assets/admin') }}/img/avatars/1.png" alt
-                            class="w-px-40 h-auto rounded-circle" />
-                    </div>
+                <a class="" href="javascript:void(0);">
+                    <span class="fw-semibold d-block">{{ Auth::guard('admin')->user()->name }} (<small class="text-muted">Admin</small>)</span>
                 </a>
-                <ul class="dropdown-menu dropdown-menu-end" style="z-index: 100">
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0 me-3">
-                                    <div class="avatar avatar-online">
-                                        <img src="{{ asset('assets/admin') }}/img/avatars/1.png" alt
-                                            class="w-px-40 h-auto rounded-circle" />
-                                    </div>
-                                </div>
-                                <div class="flex-grow-1">
-                                    <span class="fw-semibold d-block">{{ Auth::guard('admin')->user()->name }}</span>
-                                    <small class="text-muted">Admin</small>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="#">
-                            <i class="bx bx-user me-2"></i>
-                            <span class="align-middle">My Profile</span>
-                        </a>
-                    </li>
-                    {{-- <li>
-        <a class="dropdown-item" href="#">
-            <i class="bx bx-cog me-2"></i>
-            <span class="align-middle">Settings</span>
-        </a>
-        </li>
-        <li>
-        <a class="dropdown-item" href="#">
-            <span class="d-flex align-items-center align-middle">
-            <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-            <span class="flex-grow-1 align-middle">Billing</span>
-            <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-            </span>
-        </a>
-        </li> --}}
-                    <li>
-                        <div class="dropdown-divider"></div>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="{{ route('admin.auth.logout') }}">
-                            <i class="bx bx-power-off me-2"></i>
-                            <span class="align-middle">Log Out</span>
-                        </a>
-                    </li>
-                </ul>
+            </li>
+
+            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="dropdown-item" href="{{ route('admin.auth.logout') }}">
+                    <i class="bx bx-power-off me-2"></i>
+                    <span class="align-middle">Log Out</span>
+                </a>
             </li>
             <!--/ User -->
         </ul>
